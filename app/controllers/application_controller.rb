026764216561e37d299b309551c3da0a22ee4f06
katuki0120 <katuki2898@gmail.com>
2020-05-10 17:48:01 +0900

    Basic認証の導入&本番環境飲みに適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  # Basic認証のbeforeアクション ifで本番環境かそうでないかの確認をしている
+  before_action :basic_auth, if: :production?
+  protect_from_forgery with: :exception
+
+  private
+  # Basic認証を確認するためのメソッド
+  def production?
+    Rails.env.production?
+  end
+  # Basic認証のメソッド
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
 end
