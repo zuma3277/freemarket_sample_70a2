f567a7d18cadf1d3488c3080db377db49bc25af1
nazonazopp <61644930+nazonazopp@users.noreply.github.com>
2020-06-01 20:33:45 +0900

    [コードレビュー]ユーザーマイページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'messages/index'
   root "messages#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'logout/index'
+  get 'creditcard/index'
+  get 'creditcard/new'
+  get 'creditcard/create'
 end
