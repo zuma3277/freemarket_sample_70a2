1bd5fa9a23bbd841128ef3d1ca6eee2d18a7a5fe
katuki0120 <katuki2898@gmail.com>
2020-06-14 15:35:23 +0900

    messageからitem_pageへ変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "mypage#index"
   resources :logout, only: [:index]
   resources :creditcard, only:[:index, :new, :create]
+  root "item_page#index"
+  resources :item_page, only: [:index]
 end
