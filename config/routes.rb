c108440ecd3614a5494aeab3121cce32307032ea
karen-mizo <ktaisaarm_3531@icloud.com>
2020-06-15 21:16:22 +0900

    ルートの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root "mypage#index"
   resources :logout, only: [:index]
   resources :creditcard, only:[:index, :new, :create]
-  root "products#index"
   resources :products, only: [:index]
 end
