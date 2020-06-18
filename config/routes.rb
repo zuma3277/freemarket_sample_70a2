79e90ce3a7407e0b19b53c4ce570701583d9ad42
yoshiryo413 <yoshida.rio13@gmail.com>
2020-06-19 01:30:32 +0900

    修正commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "index#index"
   resources :logout, only: [:index]
   resources :creditcard, only:[:index, :new, :create]
+  resources :mypage, only: [:index]
 end
