5697f8496f427591c67b0a3d57268fd9becf4873
katuki0120 <katuki2898@gmail.com>
2020-06-25 23:10:42 +0900

    migrateファイルの記述ミス修正

--- db/migrate/20200624115309_create_comments.rb
+++ db/migrate/20200624115309_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.user_id :references, null:false, foreign_key:true
-      t.product_id :references, null:false, foreign_key:true
-      t.comment :text, null:false
+      t.references :user, null:false, foreign_key:true
+      t.references :product, null:false, foreign_key:true
+      t.text :textcomment, null:false
       t.timestamps
     end
   end
--- db/migrate/20200624122755_create_addresses.rb
+++ db/migrate/20200624122755_create_addresses.rb
@@ -1,18 +1,18 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-      t.user_id :references, null:false , foreign_key: true
-      t.destination_family_name :string , null: false
-      t.destination_first_name :string , null: false
-      t.destination_family_name_kana :string , null: false
-      t.destination_first_name_kana :string , null: false
-      t.postal_code :integer , null: false
-      t.prefecture :integer , null: false
-      t.prefecture :string , null: false 
-      t.city :string , null: false
-      t.address :string , null: false
-      t.building_name :string 
-      t.phone_namber :integer , null: false, unque:true
+      t.references :user, null:false , foreign_key: true
+      t.string :destination_family_name , null: false
+      t.string :destination_first_name , null: false
+      t.string :destination_family_name_kana , null: false
+      t.string :destination_first_name_kana , null: false
+      t.integer :postal_code , null: false
+      t.integer :prefecture , null: false
+      t.string :prefecture , null: false 
+      t.string :city , null: false
+      t.string :address , null: false
+      t.string :building_name
+      t.integer :phone_namber , null: false, unque:true
       t.timestamps
     end
   end
