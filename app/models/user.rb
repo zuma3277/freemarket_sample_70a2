b8b01a49a4ead2ad84592d531ca742ff873252a9
katuki0120 <katuki2898@gmail.com>
2020-06-27 00:30:49 +0900

    deviseを使ったuserモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-  has_one :credit_card  dependent: :destroy
-  has_one :profile  dependent: :destroy
-  has_one :address  dependent: :destroy
-  has_many :comments   dependent: :destory
-  has_one :destination  dependent: :destory
-  has_many :favorite_products dependent: :destroy
-  has_many :products dependent: :destroy
+  validates :name, presence: true, uniqueness: true
   
 end
