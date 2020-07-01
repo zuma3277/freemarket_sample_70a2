c013cc096fa7e41b8aad369921807ea100d093a6
karen-mizo <ktaisaarm_3531@icloud.com>
2020-07-01 22:53:16 +0900

    7月1日変更分

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,17 @@
 class ProductsController < ApplicationController
-  def index
+  
+  def new
     @product = Product.new
   end
 
   def create
-    @product = Product.create
+    @product = products.new(product_params)
   end
 
   private
 
   def product_params
-    params.require(:product).premit(:name,:price,:dealing_status,:products_introduction,category:[:name],brand:[:name],product_condition:[:product_condition],size:[:size],shipment:[:area,:charge_payment,:day],product_imgs:[:image])
+    params.require(:product).permit(:name,:price,:dealing_status,:products_introduction,category:[:name],brand:[:name],product_condition:[:product_condition],size:[:size],shipment:[:area,:charge_payment,:day],product_imgs:[:image])
   end
   
 end
