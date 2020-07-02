d069004e0fc3fbc001e6da220cc5c7a9bded91dc
yoshiryo413 <yoshida.rio13@gmail.com>
2020-07-02 22:43:46 +0900

    変更commit

--- app/controllers/sign_up_controller.rb
+++ app/controllers/sign_up_controller.rb
@@ -3,15 +3,20 @@ class SignUpController < ApplicationController
   end
 
 
-
-  def new
+  def sign_up
+    @user = User.new
   end
-  
-
 
   def create
+    User.create(user_params)
+    redirect_to posts_path
   end
 
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname,:email,:encrypted_password,:family_name,:first_name,:family_name_kana,:first_name_kana,:birthday,:destination_family_name,:destination_first_name,:destination_family_name_kana,:destination_first_name_kana,:postal_code,:prefecture,:prefecture,:city,:address,:building_name,:phone_number,)
+  end
 
 
 end
