72a4cc4c04a9e3fe7ad3ace3316e6a4aa0de1dda
karen-mizo <ktaisaarm_3531@icloud.com>
2020-07-05 16:37:28 +0900

    データーベース修正（溝口）

--- db/migrate/20200627145035_create_product_imgs.rb
+++ db/migrate/20200627145035_create_product_imgs.rb
@@ -1,7 +1,6 @@
 class CreateProductImgs < ActiveRecord::Migration[5.2]
   def change
     create_table :product_imgs do |t|
-      t.references :product,  null: false, foreign_key: true
       t.string :image,        null: false
       t.timestamps
     end
