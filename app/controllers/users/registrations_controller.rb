b21ba728632ec7228eb2f4009663cddee8cc9dd9
yoshiryo413 <yoshida.rio13@gmail.com>
2020-07-12 15:31:05 +0900

    ログインcomiit

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,7 +4,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
-  
+  def after_sign_up_path_for(resource)
+    comp_reg_index_path
+  end
 
   # private
   # def user_params
