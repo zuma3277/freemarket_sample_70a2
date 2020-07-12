affcd76a3ba065fdbe3adb83f1d5a9bfce6c56fb
katuki0120 <katuki2898@gmail.com>
2020-07-12 15:52:25 +0900

    ログアウト機能の実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,6 +8,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     comp_reg_index_path
   end
 
+  def after_sign_out_path_for(resource)
+    index_index_path
+  end 
   # private
   # def user_params
   #   params.require(:user).permit(:nickname,:email,:encrypted_password,:ensure_password,:family_name,:first_name,:family_name_kana,:first_name_kana,:birthday,:destination_family_name,:destination_first_name,:destination_family_name_kana,:destination_first_name_kana,:postal_code,:prefecture,:city,:address,:building_name,:phone_number)
