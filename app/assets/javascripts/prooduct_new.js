7cf0e5029787041662a62ddccc8e4abdd20f18b9
karen-mizo <ktaisaarm_3531@icloud.com>
2020-07-12 18:08:15 +0900

    削除機能

--- app/assets/javascripts/prooduct_new.js
+++ app/assets/javascripts/prooduct_new.js
@@ -26,8 +26,7 @@ $(document).on('turbolinks:load',function(){
                                         <img src='${e.target.result}' title='${file.name}' >
                                       </figure>
                                       <div class="preview-image__button">
-                                        <a class="preview-image__button__edit">編集</a>
-                                        <a class="preview-image__button__delete" data-image-id="${labelLength}">削除</a>
+                                        <a href="" class="preview-image__button__delete" data-image-id="${labelLength}">削除</a>
                                       </div>
                                     </li>`);
           $("#input-img>label").eq(-1).css('display','none');
@@ -51,4 +50,24 @@ $(document).on('turbolinks:load',function(){
   });
 });
 
-$(document).on("click",)
+$(document).on("click","preview-image__button__delete",function(){
+  let targetImageId = $(this).data('image-id');
+
+  $(`#upload-image${targetImageId}`).remove();
+
+  $(`[for=input_images${targetImageId}]`).remove();
+
+  let imageLength = $('#output-box').children('li').length;
+  if (imageLength < 9) {
+    let labelLength = $("#input-img>label").eq(-1).data('label-id');
+    // 表示されているプレビューが９以下なら、新たにinputを生成する
+    $("#input-img").append(`<label for="input-images${labelLength+1}" class="input-label" data-label-id="${labelLength+1}">
+                                <input multiple="multiple" class="input-function" id="input-images${labelLength+1}" style="display: none;" type="file" >
+                                <img class="logo_camera" src="/assets/icon_camera-24c5a3dec3f777b383180b053077a49d0416a4137a1c541d7dd3f5ce93194dee.png">
+                                <p class="input-text">
+                                  ドラックアンドドロップ
+                                  またはクリックしてファイルをダウンロード
+                                </p>
+                              </label>`);
+  };
+});
