42bcbc1a25db1cc4caeee4e27be82731ecff02c2
katuki0120 <katuki2898@gmail.com>
2020-07-15 01:22:45 +0900

    sign_up削除

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,6 @@ Rails.application.routes.draw do
   resources :item_page, only: [:index]
   resources :comp_reg, only: [:index]
   resources :products, only: [:index]
-  resources :sign_up, only: [:index, :new, :create]
   resources :login, only: [:index]
   resources :users
 end
