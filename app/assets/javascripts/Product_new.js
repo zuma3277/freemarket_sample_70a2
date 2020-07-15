db4bc02258eb673f5b6467a79f4ecc851249ed67
karen-mizo <ktaisaarm_3531@icloud.com>
2020-07-15 23:08:35 +0900

    修正

--- app/assets/javascripts/Product_new.js
+++ app/assets/javascripts/Product_new.js
@@ -50,113 +50,114 @@ $(document).on('turbolinks:load',function(){
       reader.readAsDataURL(file);
     });
   });
-});
-
-$(document).on("click","preview-image__button__delete",function(){
-  let targetImageId = $(this).data('image-id');
-
-  $(`#upload-image${targetImageId}`).remove();
-
-  $(`[for=input_images${targetImageId}]`).remove();
-
-  let imageLength = $('#output-box').children('li').length;
-  if (imageLength < 9) {
-    let labelLength = $("#input-img>label").eq(-1).data('label-id');
-    // 表示されているプレビューが９以下なら、新たにinputを生成する
-    $("#input-img").append(`<label for="input-images${labelLength+1}" class="input-label" data-label-id="${labelLength+1}">
-                                <input multiple="multiple" class="input-function" id="input-images${labelLength+1}" style="display: none;" type="file" >
-                                <img class="logo_camera" src="/assets/icon_camera-24c5a3dec3f777b383180b053077a49d0416a4137a1c541d7dd3f5ce93194dee.png">
-                                <p class="input-text">
-                                  ドラックアンドドロップ
-                                  またはクリックしてファイルをダウンロード
-                                </p>
-                              </label>`);
-  };
-});
 
 
-//商品の説明の文字カウント機能
-$(function(){
+  $(document).on("click","preview-image__button__delete",function(){
+    let targetImageId = $(this).data('image-id');
+
+    $(`#upload-image${targetImageId}`).remove();
+
+    $(`[for=input_images${targetImageId}]`).remove();
+
+    let imageLength = $('#output-box').children('li').length;
+    if (imageLength < 9) {
+      let labelLength = $("#input-img>label").eq(-1).data('label-id');
+      // 表示されているプレビューが９以下なら、新たにinputを生成する
+      $("#input-img").append(`<label for="input-images${labelLength+1}" class="input-label" data-label-id="${labelLength+1}">
+                                  <input multiple="multiple" class="input-function" id="input-images${labelLength+1}" style="display: none;" type="file" >
+                                  <img class="logo_camera" src="/assets/icon_camera-24c5a3dec3f777b383180b053077a49d0416a4137a1c541d7dd3f5ce93194dee.png">
+                                  <p class="input-text">
+                                    ドラックアンドドロップ
+                                    またはクリックしてファイルをダウンロード
+                                  </p>
+                                </label>`);
+    };
+  });
+
+
+  //商品の説明の文字カウント機能
+
   $('.input-sentences').keyup(function(){
     var count =$(this).val().length;
     $('.count').text(count);
   });
-});
 
 
 
-//カテゴリーの選択機能
-$(function(){
-  function categoryOption(category){
-    var optionHtml = `<option value="${category.id}">${category.name}</option>`;
-    return optionHtml;
-  }
-  $('#category-select-parent').on('change', function(){
-    let parentCategoryId = $(this).val();
-    if (parentCategoryId == ''){
-      $('#select-children-box').remove();
-      $('#select-grandchildren-box').remove();
-    }else{
-      $.ajax({
-        url: '/items/category_children',
-        type: 'GET',
-        data: { parent_id: parentCategoryId },
-        dataType: 'json'
-      })
-      .done(function(category_children){
+
+  //カテゴリーの選択機能
+  $(function(){
+    function categoryOption(category){
+      var optionHtml = `<option value="${category.id}">${category.name}</option>`;
+      return optionHtml;
+    }
+    $('#category-select-parent').on('change', function(){
+      let parentCategoryId = $(this).val();
+      if (parentCategoryId == ''){
         $('#select-children-box').remove();
         $('#select-grandchildren-box').remove();
-        let optionHtml = '';
-        category_children.forEach(function(child){
-          optionHtml += categoryOption(child);
+      }else{
+        $.ajax({
+          url: '/items/category_children',
+          type: 'GET',
+          data: { parent_id: parentCategoryId },
+          dataType: 'json'
+        })
+        .done(function(category_children){
+          $('#select-children-box').remove();
+          $('#select-grandchildren-box').remove();
+          let optionHtml = '';
+          category_children.forEach(function(child){
+            optionHtml += categoryOption(child);
+          });
+          $('#error-category').before(`<div class="sell-collection_select " id="select-children-box">
+                                          <label class="sell-collection_select__label" for="item_category_id">
+                                            <select class="sell-collection_select__input" id="category-select-children" required="required" name="item[category_id]">
+                                              <option value="">選択して下さい</option>
+                                              ${optionHtml}
+                                            </select>
+                                            <i class="fas fa-chevron-down"></i>
+                                          </label>
+                                        </div>`
+          );
+        })
+        .fail(function(){
+          alert('カテゴリー取得に失敗しました');
         });
-        $('#error-category').before(`<div class="sell-collection_select " id="select-children-box">
-                                        <label class="sell-collection_select__label" for="item_category_id">
-                                          <select class="sell-collection_select__input" id="category-select-children" required="required" name="item[category_id]">
-                                            <option value="">選択して下さい</option>
-                                            ${optionHtml}
-                                          </select>
-                                          <i class="fas fa-chevron-down"></i>
-                                        </label>
-                                      </div>`
-        );
-      })
-      .fail(function(){
-        alert('カテゴリー取得に失敗しました');
-      });
-    }
-  });
-  $('.sell-container__content__details').on('change', '#category-select-children', function(){
-    let childrenCategoryId = $(this).val();
-    if (childrenCategoryId == ''){
-      $('#select-grandchildren-box').remove(); 
-    }else{
-      $.ajax({
-        url: '/items/category_grandchildren',
-        type: 'GET',
-        data: { child_id: childrenCategoryId },
-        dataType: 'json'
-      })
-      .done(function(category_grandchildren){
-        $('#select-grandchildren-box').remove();
-        let optionHtml = '';
-        category_grandchildren.forEach(function(grandchildren){
-          optionHtml += categoryOption(grandchildren);
+      }
+    });
+    $('.sell-container__content__details').on('change', '#category-select-children', function(){
+      let childrenCategoryId = $(this).val();
+      if (childrenCategoryId == ''){
+        $('#select-grandchildren-box').remove(); 
+      }else{
+        $.ajax({
+          url: '/items/category_grandchildren',
+          type: 'GET',
+          data: { child_id: childrenCategoryId },
+          dataType: 'json'
+        })
+        .done(function(category_grandchildren){
+          $('#select-grandchildren-box').remove();
+          let optionHtml = '';
+          category_grandchildren.forEach(function(grandchildren){
+            optionHtml += categoryOption(grandchildren);
+          });
+          $('#error-category').before(`<div class="sell-collection_select " id="select-grandchildren-box">
+                                          <label class="sell-collection_select__label" for="item_category_id">
+                                            <select class="sell-collection_select__input" id="category-select-grandchildren" required="required" name="item[category_id]">
+                                              <option value="">選択して下さい</option>
+                                              ${optionHtml}
+                                            </select>
+                                            <i class="fas fa-chevron-down"></i>
+                                          </label>
+                                        </div>`
+          );
+        })
+        .fail(function(){
+          alert('カテゴリー取得に失敗しました');
         });
-        $('#error-category').before(`<div class="sell-collection_select " id="select-grandchildren-box">
-                                        <label class="sell-collection_select__label" for="item_category_id">
-                                          <select class="sell-collection_select__input" id="category-select-grandchildren" required="required" name="item[category_id]">
-                                            <option value="">選択して下さい</option>
-                                            ${optionHtml}
-                                          </select>
-                                          <i class="fas fa-chevron-down"></i>
-                                        </label>
-                                      </div>`
-        );
-      })
-      .fail(function(){
-        alert('カテゴリー取得に失敗しました');
-      });
-    }
+      }
+    });
   });
 });
