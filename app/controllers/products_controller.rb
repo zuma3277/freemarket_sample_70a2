3a9dfde818a157534873148adc11dd51b9e571a8
katuki0120 <katuki2898@gmail.com>
2020-07-17 19:48:22 +0900

    商品削除完了ページのhtml名を変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,17 @@ class ProductsController < ApplicationController
   def index
     
   end
+
   def new
     
   end
+
   def show
     @products = Product.order('created_at DESC').find(params[:id])
+  end
+
+  def destroy
+    #@item.destroy
+    #redirect_to  delete_products_path
   end
 end
