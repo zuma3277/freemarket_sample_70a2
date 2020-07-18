11f14f43c5ace4d0fe19adfccf1779ebfdb1ecdf
karen-mizo <ktaisaarm_3531@icloud.com>
2020-07-18 18:36:25 +0900

    データベース修正

--- db/migrate/20200628154230_create_products.rb
+++ db/migrate/20200628154230_create_products.rb
@@ -6,9 +6,9 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.integer    :price,             null: false
       t.references :category,          null: false, foreign_key: true
       t.references :brand,             null: false, foreign_key: true
-      t.references :product_condition_id, null: false
-      t.references :size_id,              null: false
-      t.integer    :dealing_status,    null: false, default: 0
+      t.integer :product_condition_id, null: false
+      t.integer :size_id,              null: false
+      t.integer :dealing_status,    null: false, default: 0
       t.integer :delivery_fee_id,      null: false
       t.integer :prefecture_id,        null: false
       t.integer :delivery_days_id,     null: false
