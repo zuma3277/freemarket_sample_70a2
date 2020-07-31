9eadb21c5e480eb4c0c3744563dfba7eb5c3aa82
nazonazopp <61644930+nazonazopp@users.noreply.github.com>
2020-07-31 22:42:58 +0900

    アソシエーションで表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -31,11 +31,11 @@ class ProductsController < ApplicationController
 
   def show
     @products = Product.order('created_at DESC').find(params[:id])
-    @size = Size.find(@products.size_id)
-    @product_condition = Product_condition.find(@products.product_condition_id)
-    @delivery_fee = Delivery_fee.find(@products.delivery_fee_id)
-    @prefecture = Prefecture.find(@products.prefecture_id)
-    @delivery_days = Delivery_days.find(@products.delivery_days_id)
+    @size = @products.size.name
+    @product_condition = @products.product_condition.name
+    @delivery_fee = @products.delivery_fee.name
+    @prefecture = @products.prefecture.name
+    @delivery_days = @products.delivery_days.name
   end
 
   def destroy
--- app/models/size.rb
+++ app/models/size.rb
@@ -1,5 +1,8 @@
 class Size < ActiveHash::Base
-
+  
   self.data = [ {id: 1, name: 'Sサイズ'}, {id: 2, name: 'Mサイズ'}, {id: 3, name: 'Lサイズ'}]
+
+  include ActiveHash::Associations
+  has_many :products
 
 end
